01a83428a8021e33d1e106fa2ed365a2c43ad3eb
Stylianos Manousakis <sm669030@broadcom.net>
2020-03-01 22:28:21 +0100

    Set up of routing to components pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {LoginComponent} from './pages/login/login.component';
+import {PostEditComponent} from './pages/post-edit/post-edit.component';
 
 
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'postedit', component: PostEditComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
