ab0a562ee4fd569888c8cc40745a0fa634c4a116
Stylianos Manousakis <sm669030@broadcom.net>
2020-03-05 12:12:04 +0100

    Moved Functionality to parent component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ServerService} from '../../services/server.service';
 import {Router} from '@angular/router';
+import {LandingComponent} from '../landing/landing.component';
 
 
 @Component({
@@ -9,59 +10,16 @@ import {Router} from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  private TABLE_DATA = [];
-  private USERS = [];
-  private POSTS = [];
-  private max;
-  constructor(private serverService: ServerService, private router: Router) { }
-  page = 1;
-  pageSize = 10;
 
-  get tableData() {
-    return this.TABLE_DATA
-      .map((post, i) => ({id: i + 1, ...post}))
-      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
-  }
-  increasePage() {
-    this.page++;
-  }
-  decreasePage() {
-    this.page--;
-  }
-  onGetPosts() {
-    this.serverService.getServerPosts().subscribe(
-      (response: any []) => {
-        this.POSTS = response;
-        this.onGetUsers();
-      },
-      (error) => console.log(error)
-    );
-  }
-  onGetUsers() {
-    this.serverService.getServerUsers().subscribe(
-      (response: any []) => {
-          this.USERS = response;
-          console.log(this.USERS);
-          console.log(this.POSTS);
-          this.buildTable(this.USERS, this.POSTS);
-      },
-      (error) => console.log(error)
-    );
-  }
-  buildTable(users, post) {
-    const tableData = [];
-    for (let i = 0; i < post.length; i++) {
-      tableData.push({user: post[i].userId, body: post[i].body , title: post[i].title });
-    }
-    for (let i = 0; i < tableData.length; i++) {
-      const id = tableData[i].user;
-      tableData[i].user = users[id - 1];
-    }
-    console.log('-----------------');
-    console.log(tableData);
-    this.TABLE_DATA = tableData;
-    this.max = this.TABLE_DATA.length / this.pageSize;
-  }
+
+  constructor(private serverService: ServerService, private router: Router, private lnd: LandingComponent) { }
+
+
+
+
+
+
+
   goToCompanySite(site) {
     const url = 'http://' + site;
     const win = window.open(url, '_blank');
@@ -70,10 +28,12 @@ export class HomeComponent implements OnInit {
   goToLogin() {
     this.router.navigateByUrl('/app/login');
   }
-  goToEdit() {
+  goToEdit(post, title) {
+    console.log(post);
+    console.log(title);
     this.router.navigateByUrl('/app/postedit');
   }
   ngOnInit() {
-    this.onGetPosts();
+
   }
 }
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ServerService} from '../../services/server.service';
 
 @Component({
   selector: 'app-landing',
@@ -6,10 +7,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-
-  constructor() { }
+  private TABLE_DATA = [];
+  private USERS = [];
+  private POSTS = [];
+   max;
+  pageSize = 10;
+  page = 1;
+  constructor(private serverService: ServerService) { }
 
   ngOnInit() {
+    this.onGetPosts();
   }
-
+  onGetPosts() {
+    this.serverService.getServerPosts().subscribe(
+      (response: any []) => {
+        this.POSTS = response;
+        this.onGetUsers();
+      },
+      (error) => console.log(error)
+    );
+  }
+  onGetUsers() {
+    this.serverService.getServerUsers().subscribe(
+      (response: any []) => {
+        this.USERS = response;
+        console.log(this.USERS);
+        console.log(this.POSTS);
+        this.buildTable(this.USERS, this.POSTS);
+      },
+      (error) => console.log(error)
+    );
+  }
+  buildTable(users, post) {
+    const tableData = [];
+    for (let i = 0; i < post.length; i++) {
+      tableData.push({user: post[i].userId, body: post[i].body , title: post[i].title });
+    }
+    for (let i = 0; i < tableData.length; i++) {
+      const id = tableData[i].user;
+      tableData[i].user = users[id - 1];
+    }
+    console.log('-----------------');
+    console.log(tableData);
+    this.TABLE_DATA = tableData;
+    this.max = this.TABLE_DATA.length / this.pageSize;
+  }
+  get tableData() {
+    return this.TABLE_DATA
+      .map((post, i) => ({id: i + 1, ...post}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+  }
+  increasePage() {
+    this.page++;
+  }
+  decreasePage() {
+    this.page--;
+  }
 }
